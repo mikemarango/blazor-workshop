aa5306696ab2e2d98eb5902522fbfe0b622a08c6
Michael Marango <mike.marango@gmail.com>
2019-07-04 20:46:03 +0300

    displaying login state - twitter signin

--- save-points/00-Starting-point/BlazingPizza.Client/ServerAuthenticationStateProvider.cs
+++ save-points/00-Starting-point/BlazingPizza.Client/ServerAuthenticationStateProvider.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -9,13 +10,30 @@ namespace BlazingPizza.Client
 {
     public class ServerAuthenticationStateProvider : AuthenticationStateProvider
     {
-        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        private readonly HttpClient _httpClient;
+
+        public ServerAuthenticationStateProvider(HttpClient httpClient)
         {
-            // Currently, this returns fake data
-            // In a moment, we'll get real data from the server
-            var claim = new Claim(ClaimTypes.Name, "Fake user");
-            var identity = new ClaimsIdentity(new[] { claim }, "serverauth");
-            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(identity)));
+            _httpClient = httpClient;
         }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var userInfo = await _httpClient.GetJsonAsync<UserInfo>("user");
+
+            var identity = userInfo.IsAuthenticated
+                ? new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, userInfo.Name) }, "serverauth")
+                : new ClaimsIdentity();
+
+            return new AuthenticationState(new ClaimsPrincipal(identity));
+        }
+        //public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        //{
+        //    // Currently, this returns fake data
+        //    // In a moment, we'll get real data from the server
+        //    var claim = new Claim(ClaimTypes.Name, "Fake user");
+        //    var identity = new ClaimsIdentity(new[] { claim }, "serverauth");
+        //    return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(identity)));
+        //}
     }
 }
